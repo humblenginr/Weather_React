bbbfd77fa253978ce6fe67417ed24e17d0fdb861
suod-NithishKarthik <nithishkarthik01@gmail.com>
2020-12-19 21:25:54 +0530

    Project done. Just basic fetch from the api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css"
+import { useRef, useState } from "react";
+import {Alert, Button, Form, Input, InputGroup, Label} from "reactstrap"
+
 
 function App() {
+  const reference = useRef();
+  const [temp, setTemp] = useState();
+  const [city,setCity] = useState();
+  const [error, setError] = useState();
+  console.log(error);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {!error && <h1 className="postion-absolute left-40">The temperature at  {city && city}  is  {temp && temp}  </h1>}
+        <Form className="mt-5 d-flex justify-content-center align-items-center ml-5 vh-100" onSubmit={(e) => {
+          setError(null)
+                e.preventDefault();
+                const city = reference.current.value;
+                setCity(city);
+                Axios.get('http://api.openweathermap.org/data/2.5/weather', {params: {'q': city, 'appid': 'f05928e5df234e00d837194ff176172f'}}).then(
+                  res => setTemp(res.data.main.temp)
+                )
+                .catch(err => {setError("Data does not exist!")
+                })
+        }}>
+          <InputGroup>
+          <Label className="mr-3"> Enter the city :   </Label>
+          <input ref={reference}></input>
+          <br></br>
+          {error && <Alert color="danger">{error}</Alert>} 
+          </InputGroup>
+          <br></br>
+          <Button color="primary ml-3" onClick={() => {console.log(reference.current.value)}}>SUBMIT</Button>
+        </Form>
     </div>
   );
 }
